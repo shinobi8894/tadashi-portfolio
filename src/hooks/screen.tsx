95509a29d3aedfe055cf1439b2fa8b3cfb953b8a
shinobi8894 <shinobi.design416@gmail.com>
2025-02-03 08:27:03 -0800

    Fixed window type error

--- src/hooks/screen.tsx
+++ src/hooks/screen.tsx
@@ -2,10 +2,17 @@
 import { useEffect, useState } from 'react';
 
 const useScreenWidth = () => {
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    const [screenWidth, setScreenWidth] = useState<number>(0); // Initialize with 0 or a default value
 
     useEffect(() => {
-        const handleResize = () => setScreenWidth(window.innerWidth);
+        const handleResize = () => {
+            if (typeof window !== 'undefined') {
+                setScreenWidth(window.innerWidth);
+            }
+        };
+
+        // Set initial width if in the browser
+        handleResize();
 
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
